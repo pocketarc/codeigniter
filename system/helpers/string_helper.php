56504ca8c5719cedb145c4c250810a8a923ab48b
Bruno Moreira <git@pocketarc.com>
2026-03-19 08:50:47 -0600

    fix: restore 'unique' and 'encrypt' aliases in random_string() removed in 3.2.0-dev.

--- system/helpers/string_helper.php
+++ system/helpers/string_helper.php
@@ -227,8 +227,10 @@ if ( ! function_exists('random_string'))
 						break;
 				}
 				return substr(str_shuffle(str_repeat($pool, ceil($len / strlen($pool)))), 0, $len);
+			case 'unique':
 			case 'md5':
 				return md5(uniqid(mt_rand()));
+			case 'encrypt':
 			case 'sha1':
 				return sha1(uniqid(mt_rand(), TRUE));
 		}
