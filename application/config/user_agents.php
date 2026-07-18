fb57d3519fa929a35e78e279c3dcdd5cc47b2456
Bruno Moreira <github@pocketarc.com>
2026-07-18 18:23:24 -0400

    fix: Detect modern Edge instead of reporting it as Chrome.
    * Update Edge user agent detection for legacy version
    
    Update Edge user agent detection for legacy version and modern version that detect Edge with keyword 'Edg'
    
    * fix: Match legacy Edge before Chromium Edge.
    
    ---------
    
    Co-authored-by: Muhammad Nobin <nobin@live.com>

--- application/config/user_agents.php
+++ application/config/user_agents.php
@@ -61,8 +61,8 @@ $platforms = array(
 $browsers = array(
 	'OPR'			=> 'Opera',
 	'Flock'			=> 'Flock',
-	'Edg' 			=> 'Edge',
-    'Edge' 			=> 'Edge Legacy',
+	'Edge'			=> 'Edge Legacy',
+	'Edg'			=> 'Edge',
 	'Chrome'		=> 'Chrome',
 	// Opera 10+ always reports Opera/9.80 and appends Version/<real version> to the user agent string
 	'Opera.*?Version'	=> 'Opera',
